3fe4b116a52ba7a2c4699d3716c2d958afbf2c0a
psarker06 <ps0035@mail.wvu.edu>
2025-10-08 02:37:22 +0000

    Deploying to gh-pages from @ psarker06/psarker06.github.io@79a08a98bb11f3d4f56136ea5d900f6a2afc5326 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "psarker06/pramiti.github.io",
+    "data-repo": "psarker06/psarker06.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
